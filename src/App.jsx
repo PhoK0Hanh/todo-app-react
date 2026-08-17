918e0ef6b1f8f018108728e36f07d31f8af3d461
PhoK0Hanh <hoangvu32048@gmail.com>
2026-08-17 21:53:08 +0700

    feat: complete lesson 1 - JSX basics

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <p>Xin chào, tôi đang học React.</p>
+      <p>Tôi sẽ xây dựng ứng dụng Todo list.</p>
+    </div>
+  );
+}
+
+export default App;
