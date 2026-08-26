08789c0b865261cae31aebeb16de18899567a495
PhoK0Hanh <hoangvu32048@gmail.com>
2026-08-26 16:28:21 +0700

    feat: complete lesson 6.6 - extract todo form

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,16 @@ import "./App.css";
 import Header from "./Header";
 import TodoItem from "./TodoItem";
 import { useState } from "react";
+import TodoForm from "./TodoForm";
 
 function App() {
   const [todos, setTodos] = useState([
     { id: 1, text: "Do exercises", completed: false },
     { id: 2, text: "Read a book", completed: false },
   ]);
-  const [todoInput, setTodoInput] = useState("");
 
-  function handleAddTodo() {
-    if (todoInput.trim() !== "") {
-      setTodos([
-        ...todos,
-        { id: Date.now(), text: todoInput, completed: false },
-      ]);
-      setTodoInput("");
-    }
+  function handleAddTodo(newTodo) {
+    setTodos([...todos, { id: Date.now(), text: newTodo, completed: false }]);
   }
   function handleDeleteTodo(id) {
     setTodos(todos.filter((todo) => todo.id !== id));
@@ -52,17 +46,7 @@ function App() {
           />
         ))}
       </ul>
-      <input
-        type="text"
-        value={todoInput}
-        onChange={(e) => setTodoInput(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            handleAddTodo();
-          }
-        }}
-      />
-      <button onClick={handleAddTodo}>Add</button>
+      <TodoForm onAdd={handleAddTodo} />
     </div>
   );
 }
---
+++ src/TodoForm.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function TodoForm({ onAdd }) {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.trim() !== "") {
+      onAdd(inputValue.trim());
+      setInputValue("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+
+export default TodoForm;
