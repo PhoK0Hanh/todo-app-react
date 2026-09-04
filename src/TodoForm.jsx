30cb9cd7208366b6d0765c97afbf36bfbc3b7139
PhoK0Hanh <hoangvu32048@gmail.com>
2026-09-04 22:14:47 +0700

    feat: complete lesson 6.13 - useRef

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,19 +1,22 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function TodoForm({ onAdd }) {
   const [inputValue, setInputValue] = useState("");
+  const inputRef = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (inputValue.trim() !== "") {
       onAdd(inputValue.trim());
       setInputValue("");
+      inputRef.current.focus();
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <input
+        ref={inputRef}
         type="text"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
